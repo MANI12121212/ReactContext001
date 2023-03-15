d96bd45dd3b6adf7ed0622b0feabd442974ccd75
MANI12121212 <101326938+MANI12121212@users.noreply.github.com>
2023-03-15 22:12:29 +0530

    context001

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Comp from './components/Comp';
+// import Toggle from './components/Toggle'
+import { useState,createContext } from "react";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from  './components/Home'
 
+
+export const ThemeContext=createContext("hello");
 function App() {
+  const [first, setfirst] = useState(true)
+  function hello(){
+    setfirst(!first)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>
+      I Have changed to {first?"light":"dark"} Mode
+    </h1>
+    <div style={first?{color:'black'}:{color:"aqua", backgroundColor:"#222222"}}>
+
+    <ThemeContext.Provider value={first}>
+      <Comp />
+    <button onClick={hello}>Changed the mode to {first?"dark":"light"}</button>
+        <Routes>
+          <Route  path='/home' element={<Home hello={hello}/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/service' element={<About/>}/>
+        </Routes>
+    </ThemeContext.Provider>
+    </div>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+ 
+import Toggle from './Toggle'
+
+const Home = ({hello}) => {
+  return (
+    <div>
+        <Toggle hello={hello}/>
+    </div>
+  )
+}
+
+export default Home
